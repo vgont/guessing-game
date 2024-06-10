7222fd8f8133e708e222c01ca667efd7e14b6383
vgont <vgontdev@gmail.com>
2024-06-09 22:33:28 -0300

    notes about gen_range and continue were added

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::{cmp::Ordering, io};
 fn main() {
     println!("Guessing Game!");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let secret_number = rand::thread_rng().gen_range(1..=100); //generates a random u32 between 1 and 100
 
     loop {
         let mut guess = String::new();
@@ -16,7 +16,10 @@ fn main() {
 
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
-            Err(_) => continue,
+            Err(_) => {
+                println!("Invalid input: {}", guess);
+                continue; //-> restarts the loop
+            }
         };
 
         match guess.cmp(&secret_number) {
